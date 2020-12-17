de818d40d1b20aa36829fa36218901f6d2b722c3
Olga Kondrashova <ol.kondraschowa@gmail.com>
2020-12-17 17:49:27 +0300

    protocol 2.0

--- client-server/HexagonProtocol.py
+++ client-server/HexagonProtocol.py
@@ -1,11 +1,10 @@
-# TODO
-# Преоразоваие словаря в json-файл
+import json
+
+example_dict = {'id': 1, 'session_id': 2, 'unit': 'army', 'coord_from': [1, 1], 'coord_to': [10, 10]}
 
 class HexagonProtocol:
-    __id_static: int = 0
-    __id: int
     __size: int = 0
-    __data: list
+    __data: dict
 
     __coding: str = 'utf-8'
 
@@ -15,14 +14,8 @@ class HexagonProtocol:
     __bytes_end_encrypted: bytes = b'end_enc'
     __bytes_sep: bytes = b':'
 
-    def __init__(self, data: list):
-        self.__id = HexagonProtocol.__id_static
-        HexagonProtocol.__id_static += 1
-
-        self.__data = data[:]
-
-    def getId(self):
-        return self.__id
+    def __init__(self, data: dict):
+        self.__data = data
 
     def getSize(self):
         return self.__size
@@ -31,27 +24,21 @@ class HexagonProtocol:
         return self.__data
 
     def getByteProtocol(self):
-        res_str = self.__createByteStr(self.__bytes_start, self.__bytes_end, self.__bytes_sep)
-        return res_str
+        return self.__createByteStr(self.__bytes_start, self.__bytes_end, self.__bytes_sep)
 
     def getByteProtocolEncrypted(self):
         return self.__createByteStr(self.__bytes_start_encrypted, self.__bytes_end_encrypted, self.__bytes_sep)
 
     def __str__(self):
         return 'Object of class HexagonProtocol \n' + \
-               'id = ' + str(self.__id) + '\n' + \
                'data = ' + str(self.__data) + '\n'
 
     def __createByteStr(self, bytes_start, bytes_end, bytes_sep):
-        # TODO подсчет size сообщения
         res_str = bytes_start + \
-                  str(self.__id).encode(self.__coding) + \
                   bytes_sep + \
-                  str(self.__size).encode(self.__coding) + \
+                  json.dumps(self.__data).encode(self.__coding) + \
                   bytes_sep + \
-                  str(self.__data).encode(self.__coding) + \
                   bytes_end
-
         return res_str
 
     @staticmethod
@@ -60,17 +47,12 @@ class HexagonProtocol:
         if HexagonProtocol.__bytes_start == byte_str[:len(HexagonProtocol.__bytes_start)] and \
                 HexagonProtocol.__bytes_end == byte_str[-len(HexagonProtocol.__bytes_end):]:
             # message isn't encrypted
-            message = byte_str[len(HexagonProtocol.__bytes_start):-len(HexagonProtocol.__bytes_end)].split(HexagonProtocol.__bytes_sep)
-            print(message)
+            message = json.loads(byte_str[len(HexagonProtocol.__bytes_start)+1\
+                                          :-len(HexagonProtocol.__bytes_end)-1].decode(HexagonProtocol.__coding))
+            return message
 
-            # TODO проверить размер сообщения
-
-        elif HexagonProtocol.__bytes_start_encrypted == byte_str[:len(HexagonProtocol.__bytes_start_encrypted)] and \
-                HexagonProtocol.__bytes_end_encrypted == byte_str[-len(HexagonProtocol.__bytes_end_encrypted):]:
-            # message is encrypted
-            a = 0
         else:
             is_HexagonProtocol = False
 
         if not is_HexagonProtocol:
-            print('Unknown protocol')
+            return 'Unknown protocol'
